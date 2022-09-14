803ab5a0871cd7cd9cb7211d678d2d0340459e7f
DmitriyPonochevnyy <pdv_75@mail.ru>
2022-09-14 23:38:05 +0500

    add Main, add README

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5, 7, 4};
+        SalesManager salesManager = new SalesManager(arr);
+        int max = salesManager.max();
+        System.out.println(max);
+    }
+}
