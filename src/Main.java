a44a26afe56e2789bae91a9ebf89327b44abf0bf
DmitriyPonochevnyy <pdv_75@mail.ru>
2022-09-28 22:25:25 +0500

    replacement int by long

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {5, 7, 4};
+        long[] arr = {5, 7, 4};
         SalesManager salesManager = new SalesManager(arr);
-        int max = salesManager.max();
+        long max = salesManager.max();
         System.out.println(max);
     }
 }
